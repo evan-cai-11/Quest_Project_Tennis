1902f2c74c88b77b48ede4e949b146fc3d90c1de
evan-cai-11 <evanzhcai@gmail.com>
2025-02-18 16:02:24 -0800

    Added pose detection for the comparison frames and got 1/2 angles that I aim to get

--- tkinter/mps_QAVS(current).py
+++ tkinter/mps_QAVS(current).py
@@ -12,7 +12,6 @@ from PyQt5.QtCore import Qt, QUrl, QTimer
 import os
 from ultralytics import YOLO
 
-# Load YOLOv8 model for person detection
 model = YOLO("yolov8m")
 
 class CustomVideoSurface(QAbstractVideoSurface):
@@ -23,14 +22,22 @@ class CustomVideoSurface(QAbstractVideoSurface):
         
         self.second_to_last_overlap = False
         self.previous_overlap = False
+        self.current_overlap = True
+
+        self.frames = []
+        self.size = 0
 
         self.screenshots_number = 1
 
-        # Initialize MediaPipe and OpenCV
         self.mp_pose = mp.solutions.pose
         self.pose = self.mp_pose.Pose()
         self.mp_drawing = mp.solutions.drawing_utils
+
+        self.position_history = []
         
+        self.backhand_prep = False
+        self.forehand_prep = False
+
     def start(self, format):
         self.format = format
         return super().start(format)
@@ -48,9 +55,7 @@ class CustomVideoSurface(QAbstractVideoSurface):
 
         boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
         boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
-
-        unionArea = boxAArea + boxBArea - interArea
-
+        
         if (boxAArea > boxBArea):
             iou = interArea / boxBArea if boxBArea != 0 else 0
         else: 
@@ -81,7 +86,6 @@ class CustomVideoSurface(QAbstractVideoSurface):
         self.screenshots_number += 1
     
     def objectDetection(self, frame_rgb):
-        # Convert the frame (image) to a format that YOLOv8 can process
         results = model(frame_rgb, device = "mps", verbose = False)[0]
         
         self.contact_detected = False
@@ -99,14 +103,11 @@ class CustomVideoSurface(QAbstractVideoSurface):
         self.racket_left = None
         self.person_left = None
         
-
-        # Draw the bounding boxes
         for det in results:
             xmin, ymin, xmax, ymax = det.boxes.xyxy[0].tolist()
             conf = det.boxes.conf[0].item()
             cls = det.boxes.cls[0].item()
 
-            # Filter out low-conf detections
             if conf > 0.5:
                 label = model.names[int(cls)]
                 if label == "tennis racket":
@@ -132,7 +133,6 @@ class CustomVideoSurface(QAbstractVideoSurface):
             cv2.rectangle(frame_rgb, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
             cv2.putText(frame_rgb, f'Person: {conf:.2f}', (int(xmin), int(ymin - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-        # Check for overlap between racket and ball
         if racket_box and ball_box:
             iou_ball = self.calculate_iou(racket_box, ball_box)
             iou_racket = self.calculate_iou(racket_box, person_box)
@@ -145,30 +145,39 @@ class CustomVideoSurface(QAbstractVideoSurface):
                 
                 if person_box:
                     self.screenshot(person_box, racket_box, frame_rgb)
-            
-        #     self.second_to_last_overlap = self.previous_overlap
-        #     self.previous_overlap = current_overlap
-        # else:
-        #     self.second_to_last_overlap = False
-        #     self.previous_overlap = False
 
-        # Check for preparation 
-        # if racket_box and ball_box and len(self.frames) > 1:
-        #     if racket_left > person_left:
-        #         print("Backhand Preparation Detected")
-        #     elif racket_right > person_right:
-        #         print("Forehand Preparation Detected")
+            # if racket_box and ball_box:
+            #     if racket_left > person_left and self.forehand_prep == True:
+            #         print("Forehand Finish Detected")
+            #     elif racket_right > person_right and self.backhand_prep == True:
+            #         print("Backhand Finish Detected")
+
+            # if racket_box and person_box:
+            #     racket_left = racket_box[0]
+            #     person_left = person_box[0]
+                
+            #     self.position_history.append((racket_left, person_left))
+                
+            #     if len(self.position_history) >= 3:
+            #         third_to_last = self.position_history[-3]
+            #         racket_left_past, person_left_past = third_to_last
+                    
+            #         if racket_left_past < person_left_past:
+            #             self.backhand_prep = True
+            #         elif racket_left_past > person_left_past:
+            #             self.forehand_prep = True
+
+            # self.frames.append(frame_rgb)
+            # self.size += 1
 
     def present(self, frame):
         if not frame.isValid():
             return False
 
-        # Convert QVideoFrame to QImage
         image = frame.image()
         if image.isNull():
             return False
-        
-        # Convert QImage to OpenCV format
+
         width = image.width()
         height = image.height()
         ptr = image.bits()
@@ -179,24 +188,19 @@ class CustomVideoSurface(QAbstractVideoSurface):
 
         self.objectDetection(frame_rgb)
 
-        # Perform pose estimation using MediaPipe
         results_pose = self.pose.process(frame_rgb)
 
-        # Draw skeleton if pose is detected
         if results_pose.pose_landmarks:
             self.mp_drawing.draw_landmarks(frame_rgb, results_pose.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
 
-        # Update the widget with the modified frame
         if self.widget:
             pixmap = QPixmap.fromImage(modified_image)
             
-            # Calculate scaled size while maintaining aspect ratio
             widget_size = self.widget.size()
             scaled_pixmap = pixmap.scaled(widget_size, 
                                         Qt.KeepAspectRatio, 
                                         Qt.SmoothTransformation)
             
-            # Center the pixmap in the label
             self.widget.setPixmap(scaled_pixmap)
 
         return True
@@ -246,6 +250,53 @@ class ComparisonWindow(QWidget):
         layout.addLayout(comparison_layout)
         self.setLayout(layout)
 
+    def calculate_angle(self, first, mid, end):
+        first = np.array(first)
+        mid = np.array(mid)
+        end = np.array(end)
+
+        radians = np.arctan2(end[1] - mid[1], end[0] - mid[0]) - np.arctan2(first[1] - mid[1], first[0] - mid[0])
+        angle = np.abs(radians * 180.0 / np.pi)
+
+        if angle > 180.0:
+            angle = 360 - angle
+        
+        return angle
+
+    def draw_pose_on_image(self, image_path):
+        image = cv2.imread(image_path)
+        if image is not None:
+            frame_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+            results_pose = self.pose.process(frame_rgb)
+
+            if results_pose.pose_landmarks:
+                self.mp_drawing.draw_landmarks(frame_rgb, results_pose.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+                
+            try:
+                landmarks = results_pose.pose_landmarks.landmark
+                
+                self.right_shoulder = [landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                self.right_hip = [landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].x, landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                self.right_elbow = [landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+
+                height, width = frame_rgb.shape[:2]
+                self.right_hip = tuple(np.multiply(self.right_hip, [width, height]).astype(int))
+                self.right_shoulder = tuple(np.multiply(self.right_shoulder, [width, height]).astype(int))
+                self.right_elbow = tuple(np.multiply(self.right_elbow, [width, height]).astype(int))
+
+                angle = self.calculate_angle(self.right_hip, self.right_shoulder, self.right_elbow)
+
+                cv2.putText(frame_rgb, str(angle), self.right_shoulder, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            except:
+                pass
+
+            frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
+            pose_image_path = os.path.splitext(image_path)[0] + "_pose.png"
+            cv2.imwrite(pose_image_path, frame_bgr)
+
+            return pose_image_path
+
     def update_screenshot(self, path):
         self.screenshot_paths.append(path)
         self.user_dropdown.addItem(f"Screenshot {len(self.screenshot_paths)}")
@@ -258,8 +309,9 @@ class ComparisonWindow(QWidget):
         }
 
         user_path = user_photos.get(selection)
-        if user_path and os.path.exists(user_path):
-            user_photo = QPixmap(user_path)
+        updated_user_path = self.draw_pose_on_image(user_path)
+        if user_path and os.path.exists(updated_user_path):
+            user_photo = QPixmap(updated_user_path)
             self.screenshot_label.setPixmap(user_photo.scaled(400, 400, Qt.KeepAspectRatio))
         else:
             self.screenshot_label.clear()
@@ -269,10 +321,11 @@ class ComparisonWindow(QWidget):
             "Rafa": "/Users/yizhengc/dev/Quest_Project_Tennis/images/rafa_forehand_contact_ao.png",
             "Sinner": "/Users/yizhengc/dev/Quest_Project_Tennis/images/sinner_forehand_contact.png"
         }
-        
+
         pro_path = pro_photos.get(player)
-        if pro_path and os.path.exists(pro_path):
-            comparison_photo = QPixmap(pro_path)
+        updated_pro_path = self.draw_pose_on_image(pro_path)
+        if pro_path and os.path.exists(updated_pro_path):
+            comparison_photo = QPixmap(updated_pro_path)
             self.comparison_photo_label.setPixmap(comparison_photo.scaled(400, 400, Qt.KeepAspectRatio))
         else:
             self.comparison_photo_label.clear()
